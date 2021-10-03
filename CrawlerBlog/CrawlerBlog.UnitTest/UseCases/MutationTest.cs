16efdb34de1d414e6c6255fefe96d9881c378729
Binh <binh.voquoc@gmail.com>
2021-10-04 02:23:51 +0700

    init addpost unit test

--- CrawlerBlog/CrawlerBlog.UnitTest/UseCases/MutationTest.cs
+++ CrawlerBlog/CrawlerBlog.UnitTest/UseCases/MutationTest.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Application.Interfaces;
+using Application.UseCases.Commands;
+using Application.UseCases.Commands.Handler;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,6 +18,12 @@ namespace CrawlerBlog.UnitTest.UseCases
         [Fact]
         public async Task AddPostHandler_ThrowException_WhenNodeReturnNull()
         {
+            var mockContext = new Mock<IApplicationDbContext>();
+            var mockLogger = new Mock<ILogger<AddPostHandler>>();
+            var service = new AddPostHandler(mockContext.Object, mockLogger.Object);
+
+            //Act
+            var result = service.Handle(new AddPostsCommand(), It.IsAny<CancellationToken>());
 
         }
 
