ea39af5294b7aadb49d0d179577a85bc073fa243
Binh <binh.voquoc@gmail.com>
2021-10-08 18:48:55 +0700

    add behaviour

--- CrawlerBlog/src/Application/DependencyInjection.cs
+++ CrawlerBlog/src/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Common.Behaviours;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -15,10 +16,7 @@ namespace Application
         {
 
             services.AddMediatR(Assembly.GetExecutingAssembly());
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
 
             return services;
         }
