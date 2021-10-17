e0ed87724d705dd9026309cec497433b647943d7
Binh <binh.voquoc@gmail.com>
2021-10-18 06:55:57 +0700

    update const

--- CrawlerBlog/src/Application/Contants/CustomErrorMessages.cs
+++ CrawlerBlog/src/Application/Contants/CustomErrorMessages.cs
@@ -9,6 +9,7 @@ namespace Application.Contants
     public static class CustomErrorMessages
     {
         public const string NoPostToUpdate = "No posts are update";
+        public const string NoCommentToUpdate = "No comments are update";
         public const string InvaildPageInput = "pageIndex and pageSize must be positive value";
         public const string WrongDomain = "Wrong domain";
         public const string ClassChanged = "Classes changed or can not get html";
